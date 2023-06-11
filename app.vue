3e9367f12d9b5fb3f0e9663e5a7f7820f76cc05d
alexander-rgb <kingofalex007@yahoo.com>
2023-06-11 22:20:50 +0700

    landingpeg

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <a class="flex justify-center my-auto h-screen" href="https://www.spinterus.com"><img src="./assets/gambar.jpg"></a>
+    <a class="flex justify-center my-auto h-screen" href="https://selalubukaterus.com/"><img src="./assets/gambar.jpg"></a>
   </div>
 </template>
 
