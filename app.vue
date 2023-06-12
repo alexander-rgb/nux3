f5a05504a27d56515878e57e0dcf519fc43958d9
alexander-rgb <kingofalex007@yahoo.com>
2023-06-12 22:01:57 +0700

    sudah clear

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <a class="flex justify-center my-auto h-screen" href="https://ocistok.com/blogs/news/pusat-grosir-baju-anak-import-murah-tangan-pertama?gclid=CjwKCAjwhJukBhBPEiwAniIcNVBgRRlSQkddGhve63GHC3vTcjBqXETfwhC05QVGsYuZw9qSt7nTDBoC-G8QAvD_BwE/"><img src="./assets/gambar2.jpg"></a>
+    <a class="flex justify-center my-auto h-screen" href="https://ocistok.com/blogs/news/pusat-grosir-baju-anak-import-murah-tangan-pertama?gclid=CjwKCAjwhJukBhBPEiwAniIcNVBgRRlSQkddGhve63GHC3vTcjBqXETfwhC05QVGsYuZw9qSt7nTDBoC-G8QAvD_BwE/"><img src="./assets/gambar2.png"></a>
   </div>
 </template>
 
