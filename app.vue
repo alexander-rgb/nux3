cafecc9d3aef6d48ce05f9288a462e341a045b2e
alexander-rgb <kingofalex007@yahoo.com>
2023-06-13 20:10:29 +0700

    asd

--- app.vue
+++ app.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <a class="flex justify-center my-auto h-screen" href="selalubukaterus.com"><img src="./assets/gambar2.png"></a>
+    <a class="flex justify-center my-auto h-screen" href="https://selalubukaterus.com/"><img src="./assets/gambar2.png"></a>
   </div>
 </template>
 
